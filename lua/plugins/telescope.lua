fc6d59ac4e900738bd772cb733d6b8842908fd52
Andrey Sologub <sologub.a@mailbox.org>
2025-04-18 16:21:09 +0200

    first commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<C-p>', function()
+      builtin.find_files({ cwd = vim.fn.getcwd() })
+    end, {})
+    vim.keymap.set('n', '<leader>fg', function()
+      builtin.live_grep({ cwd = vim.fn.getcwd() })
+    end, {})
+  end
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown',
+        'markdown_inline', 'python', 'query', 'vim', 'vimdoc'
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      }
+    })
+  end
+}
