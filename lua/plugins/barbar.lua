02decc6e2d28318caddcfef60ca7b9b73e377de2
Andrey Sologub <sologub.a@mailbox.org>
2025-04-25 13:14:48 +0200

    Fixed shortcuts and removed debugging tool

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,18 +1,21 @@
 return {
-	"romgrk/barbar.nvim",
-	dependencies = {
-		"lewis6991/gitsigns.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	init = function()
-		vim.g.barbar_auto_setup = false
+  "romgrk/barbar.nvim",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
 
-		vim.keymap.set("n", "<Leader>h", "<Cmd>BufferPrevious<CR>", {})
-		vim.keymap.set("n", "<Leader>l", "<Cmd>BufferNext<CR>", {})
-		vim.keymap.set("n", "<Leader>x", "<Cmd>BufferClose<CR>", {})
-	end,
-	opts = {
-		animation = true,
-		insert_at_start = true,
-	},
+    vim.keymap.set("n", "<C-h>", "<Cmd>BufferPrevious<CR>", {})
+    vim.keymap.set("n", "<C-l>", "<Cmd>BufferNext<CR>", {})
+    vim.keymap.set("n", "<C-x>", "<Cmd>BufferClose<CR>", {})
+  end,
+  opts = {
+    animation = true,
+    insert_at_start = true,
+    sidebar_filetypes = {
+      ["neo-tree"] = { event = "BufWinLeave", text = "File Explorer" },
+    },
+  },
 }
