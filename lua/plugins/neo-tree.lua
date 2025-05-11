7ca02ad1804b7c06a578d44a831e7dcdcb519b60
Andrey Sologub <sologub.a@mailbox.org>
2025-05-11 17:37:34 +0200

    Add Markdown preview

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,22 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
   },
   lazy = false,
   config = function()
-    require('neo-tree').setup({
+    require("neo-tree").setup({
       filesystem = {
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
-          hide_gitignored = false
+          hide_gitignored = false,
+          never_show = {
+            ".DS_Store",
+          },
         },
         follow_current_file = {
           enabled = true,
@@ -22,40 +25,39 @@ return {
       default_component_configs = {
         git_status = {
           symbols = {
-            added     = "",
-            modified  = "",
-            deleted   = "",
-            renamed   = "➜",
+            added = "",
+            modified = "",
+            deleted = "",
+            renamed = "➜",
             untracked = "★",
-            ignored   = "◌",
-            unstaged  = "✗",
-            staged    = "✓",
-            conflict  = "",
-          }
-        }
-      }
+            ignored = "◌",
+            unstaged = "✗",
+            staged = "✓",
+            conflict = "",
+          },
+        },
+      },
     })
 
     -- Toggle Neo-tree
-    vim.keymap.set('n', '<C-n>', function()
+    vim.keymap.set("n", "<C-n>", function()
       local neotree_open = false
       for _, win in ipairs(vim.api.nvim_list_wins()) do
         local buf = vim.api.nvim_win_get_buf(win)
-        local ft = vim.api.nvim_buf_get_option(buf, 'filetype')
-        if ft == 'neo-tree' then
+        local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+        if ft == "neo-tree" then
           neotree_open = true
           break
         end
       end
       if neotree_open then
-        vim.cmd('Neotree close')
+        vim.cmd("Neotree close")
       else
-        vim.cmd('Neotree filesystem reveal left')
+        vim.cmd("Neotree filesystem reveal left")
       end
     end, { noremap = true, silent = true })
 
-    vim.keymap.set('n', '<leader>e', ':Neotree filesystem focus left<CR>')
-    vim.keymap.set('n', '<leader>f', '<C-w>p', { desc = "Focus previous window" })
-
+    vim.keymap.set("n", "<leader>e", ":Neotree filesystem focus left<CR>")
+    vim.keymap.set("n", "<leader>f", "<C-w>p", { desc = "Focus previous window" })
   end,
 }
